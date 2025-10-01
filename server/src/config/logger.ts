26624713dc9fef314ca15a22b72b46e5260840db
Nikita-Ardashev <nikitos2525halk@gmail.com>
2025-10-01 23:39:38 +0400

    Исправил ошибки с билдом

--- server/src/config/logger.ts
+++ server/src/config/logger.ts
@@ -1,6 +1,8 @@
 import pino from 'pino';
 
-require('child_process').execSync('chcp 65001', { stdio: 'inherit' });
+if (process.env.NODE_ENV === 'development') {
+	require('child_process').execSync('chcp 65001', { stdio: 'inherit' });
+}
 
 export const logger = pino({
 	transport: {
@@ -8,7 +10,7 @@ export const logger = pino({
 			{ target: 'pino-pretty', options: { colorize: true } },
 			{
 				target: 'pino/file',
-				options: { destination: `${__dirname}/../logs/app.log` },
+				options: { destination: `${__dirname}/../logs/app.log`, mkdir: true },
 			},
 		],
 	},
