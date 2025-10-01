1de727f4dda9986b90176785337eac8a944d2006
Nikita-Ardashev <nikitos2525halk@gmail.com>
2025-10-01 20:13:53 +0400

    Изменил env переменные БД Добавил index.ts для vercel

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,10 +5,10 @@ import { logger } from './logger';
 configDotenv();
 
 const pool = new Pool({
-	host: process.env.POSTGRESQL_HOST,
-	user: process.env.POSTGRESQL_USER,
-	password: process.env.POSTGRESQL_PASSWORD,
-	database: process.env.POSTGRESQL_DB,
+	host: process.env.POSTGRES_URL,
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DATABASE,
 });
 
 pool.on('connect', () => {
